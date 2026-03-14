9f85fbcaeb90367d7610d2b30c034f7b7bc8daa6
tporchilai <tporchilai@gmail.com>
2026-03-15 00:52:11 +0530

    Add region awareness

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const region = process.env.AWS_REGION || 'local';
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS!');
+  res.send(`Hello from ${region}!`);
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port} in region ${region}`);
 });
 
